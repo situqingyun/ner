181aae931da77b6c3abf81e9cba5b8bf2bf546e5
chenchen <2315870098@qq.com>
2021-01-31 21:43:26 +0800

    初始化程序

---
+++ prepare_processing.py
@@ -0,0 +1,61 @@
+import json
+from util.common_util import find_all
+
+
+train_file = 'data/bmes_train.json'
+test_file = 'data/bmes_test.json'
+
+train_file_processed = 'data/processed/bmes_train.csv'
+
+def prepare_data():
+
+    # print(json.load(open(train_file, 'r')))
+    train_data = json.load(open(train_file, 'r'))
+    test_data = json.load(open(test_file, 'r'))
+    entities_dict = dict()
+    for i in train_data:
+        entities = i.get('entities', [])
+        for e in entities:
+            label = e.split('-')[1]
+            entities_dict[label] = entities_dict.get(label, 0)+1
+            # entities_set.add(e.split('-')[1])
+    print(entities_dict)
+    print('training data\'s length: {}'.format(len(train_data)))
+    print('testing data\'s length: {}'.format(len(test_data)))
+
+def label_data():
+    train_data = json.load(open(train_file, 'r'))
+    # test_data = json.load(open(test_file, 'r'))
+
+    texts=[]
+    results = []
+
+    for i in train_data:
+        entities = i.get('entities', [])
+        text = i.get('text', '')
+        texts.append(list(text))
+        # labels_indices = dict()
+        result = ['OTH']*len(text)
+        for e in entities:
+            es = e.split('-')
+            entity = es[0]
+            label = es[1]
+            start_indices = find_all(text, entity)
+            for idx in start_indices:
+                result[idx]='B-'+label
+                if len(entity)>1:
+                    for d in range(idx+1,idx+len(entity)):
+                        result[d] = 'I-'+label
+        # i['label']=','.join(result)
+        results.append(result)
+
+    with open('data/processed/train.txt', 'w') as f:
+        for i in range(len(texts)):
+            for j in range(len(texts[i])):
+                f.write(texts[i][j]+' '+results[i][j]+'\n')
+            f.write('\n')
+
+
+if __name__=='__main__':
+    # prepare_data()
+    label_data()
---
+++ util/common_util.py
@@ -0,0 +1,29 @@
+'''
+    LAK：lake，湖泊
+    RES：水库
+    ORG：机构
+    LOC：地区
+    TER：水利术语
+    PER：人员
+    DAM：大坝
+    HYD：水电站
+    RIV：河流
+    OTH：其它
+
+    training data's length: 4919
+    testing data's length: 628
+'''
+labels = ['LAK', 'RES', 'ORG', 'LOC', 'TER', 'PER', 'DAM', 'HYD', 'RIV', 'OTH']
+label_dict = {'RIV': 6340, 'LAK': 1293, 'LOC': 6638, 'TER': 1567, 'RES': 629, 'OTH': 51, 'HYD': 169, 'DAM': 107, 'ORG': 284, 'PER': 81}
+
+def find_all(s, sub):
+    index_list = []
+    index = s.find(sub)
+    while index != -1:
+        index_list.append(index)
+        index = s.find(sub, index + 1)
+
+    if len(index_list) > 0:
+        return index_list
+    else:
+        return -1
